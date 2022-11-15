40173ceb75a0f3e0a146883d3db3062051ce15af
timogoossens <r0891940@student.thomasmore.be>
2022-11-15 19:37:41 +0100

    aanpassen api

--- app/API_project.py
+++ app/API_project.py
@@ -4,8 +4,8 @@ from fastapi import FastAPI
 from random import randint
 
 app = FastAPI()
-player = [{"naam": aisaacson0, "mmr": 2436, "level": 208}, {"naam": bnardoni1, "mmr": 3759, "level": 1141},
-          {"naam": jklugman2, "mmr": 1960, "level": 1023}]
+player = [{"naam": "aisaacson0", "mmr": 2436, "level": 208}, {"naam": "bnardoni1", "mmr": 3759, "level": 1141},
+          {"naam": "jklugman2", "mmr": 1960, "level": 1023}]
 
 
 @app.get("/percentage")
