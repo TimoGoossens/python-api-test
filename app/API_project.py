fd6be26223aae74cd71a08a6d989357de5612158
TimoGoossens <r0891940@student.thomasmore.be>
2022-11-16 21:06:08 +0100

    Update API_project.py

--- app/API_project.py
+++ app/API_project.py
@@ -4,16 +4,15 @@ from random import randint
 from fastapi import FastAPI
 
 app = FastAPI()
-player = [{'naam': "aisaacson0", "mmr": 2436, "level": 208}, {'naam': "bnardoni1", "mmr": 3759, "level": 1141},
+players = [{'naam': "aisaacson0", "mmr": 2436, "level": 208}, {'naam': "bnardoni1", "mmr": 3759, "level": 1141},
           {'naam': "jklugman2", "mmr": 1960, "level": 1023}]
 
 
-@app.get("/player")
-async def get_player():
-    return random.choice(player)
-
-@app.get("/player/name")
-async def get_player_name():
-    random_name = random.choice(player)
-
-    return random_name["name"] 
+@app.get("/players/all")
+async def return_all_players():
+  return players
+@app.get("/player/data/{name}")
+async def return_specific_player(name: str):
+  for player in players:
+     if player.get("naam") == name:
+        return player
