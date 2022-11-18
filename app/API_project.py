ebf0cdc54ae70784fce5f03608b8e9c26e399c33
TimoGoossens <r0891940@student.thomasmore.be>
2022-11-18 09:25:09 +0100

    push changes

--- app/API_project.py
+++ app/API_project.py
@@ -1,7 +1,7 @@
 import random
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 origins = ["*"]
@@ -15,7 +15,7 @@ app.add_middleware(
 )
 
 
-class API_base(BaseModel):
+class PlayerCard(BaseModel):
     name: str
     mmr: int
     level: int
@@ -25,25 +25,30 @@ player1 = {"name": "aisaacson0", "mmr": 2436, "level": 208}
 player2 = {"name": "bnardoni1", "mmr": 3759, "level": 1141}
 player3 = {"name": "jklugman2", "mmr": 1960, "level": 1023}
 
-players = [player1, player2, player3]
+all_players = [player1, player2, player3]
 
-ply1 = {0: player1}
-ply2 = {0: player2}
-ply3 = {0: player3}
+players = {0: player1}
 
 
 @app.get("/players/all")
 async def return_all_players():
-    return players
+    return all_players
 
 
 @app.get("/players/random")
 async def return_random_player():
-    return random.choice(players)
+    return random.choice(all_players)
 
 
 @app.get("/player/data/{name}")
 async def return_specific_player(name: str):
-    for player in players:
+    for player in all_players:
         if player.get("name") == name:
             return player
+
+
+@app.post("/players/all")
+async def create_player(player: PlayerCard):
+    new_key = max(players, key=players.get)+1
+    players[new_key] = player
+    return all_players[new_key]
